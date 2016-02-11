673e139170042a78c3b5f3ff53cb5f24ab68c6c6
Tomasz <tomasz.kusmierczyk@letsweb.biz>
2016-02-11 14:31:43 +0100

    thymeleaf resolver as an singleton

--- src/main/java/biz/letsweb/tuckey/thymeleaf/servlets/ThymeleafConfig.java
+++ src/main/java/biz/letsweb/tuckey/thymeleaf/servlets/ThymeleafConfig.java
@@ -2,6 +2,7 @@ package biz.letsweb.tuckey.thymeleaf.servlets;
 
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+import org.thymeleaf.templateresolver.TemplateResolver;
 
 /**
  *
@@ -9,18 +10,26 @@ import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
  */
 public class ThymeleafConfig {
 
+    private static TemplateResolver TEMPLATE_RESOLVER;
+
     public static TemplateEngine getConfiguredTemplateEngine() {
-        ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver();
-        // XHTML is the default mode, but we will set it anyway for better understanding of code
-        templateResolver.setTemplateMode("XHTML");
-        templateResolver.setPrefix("/html/");
-//        templateResolver.setPrefix("/WEB-INF/classes/templates/");
-        templateResolver.setSuffix(".html");
-        templateResolver.setTemplateMode("HTML5");
-        templateResolver.setCacheTTLMs(3600000L);
-        TemplateEngine templateEngine = new TemplateEngine();
-        templateEngine.setTemplateResolver(templateResolver);
+        initializeTemplateResolverOnce();
+        final TemplateEngine templateEngine = new TemplateEngine();
+        templateEngine.setTemplateResolver(TEMPLATE_RESOLVER);
         return templateEngine;
     }
 
+    private static void initializeTemplateResolverOnce() {
+        if (TEMPLATE_RESOLVER == null) {
+//            TEMPLATE_RESOLVER = new FileTemplateResolver();
+            TEMPLATE_RESOLVER = new ServletContextTemplateResolver();
+            // XHTML is the default mode, but we will set it anyway for better understanding of code
+            TEMPLATE_RESOLVER.setTemplateMode("XHTML");
+//            TEMPLATE_RESOLVER.setPrefix("c:\\Users\\toks\\Documents\\NetBeansProjects\\spa\\front-end\\dist\\html\\");
+            TEMPLATE_RESOLVER.setPrefix("/html/");
+            TEMPLATE_RESOLVER.setSuffix(".html");
+            TEMPLATE_RESOLVER.setTemplateMode("HTML5");
+            TEMPLATE_RESOLVER.setCacheTTLMs(3600000L);
+        }
+    }
 }
